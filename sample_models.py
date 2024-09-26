3bd090e97ea7bf24c728d63f9b8b218b25b81c93
Nathan Anecone <nanecone@gmail.com>
2024-09-26 01:39:57 -0400

    add docstring for IrisNN

--- sample_models.py
+++ sample_models.py
@@ -1,8 +1,28 @@
 import torch
 import torch.nn as nn
 from model_config import config_iris
-    
+
 class IrisNN(nn.Module):
+    """
+        We're using the a model based on the classic iris dataset as a prototype.
+        It haps the advantage of being a very well studied dataset with distinguished clusters.
+    Its general features are:
+    1.	Number of Instances:
+	•	The dataset contains 150 instances (rows).
+	2.	Number of Classes:
+	•	There are three classes (species of iris flowers):
+	•	Iris setosa
+	•	Iris versicolor
+	•	Iris virginica
+	•	Each class has 50 instances.
+	3.	Number of Features:
+	•	The dataset has four features (attributes) for each instance:
+	•	Sepal length (in centimeters)
+	•	Sepal width (in centimeters)
+	•	Petal length (in centimeters)
+	•	Petal width (in centimeters)
+	•	All features are continuous numeric values.
+    """
     def __init__(self):
         super(IrisNN, self).__init__()
         input_size = config_iris.input_size
